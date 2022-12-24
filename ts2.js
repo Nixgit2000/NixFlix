5e7fbf468dd50dd83a2113259f32af10a42413d7
Nixgit2000 <fourwaycoordination@gmail.com>
2022-12-24 11:15:23 -0500

    commit

--- ts2.js
+++ ts2.js
@@ -1,7 +1,43 @@
 var enlargeFont = document.getElementById("header");
+var enlargeFont1 = document.getElementById("title1");
+var enlargeFont2 = document.getElementById("title2");
+var enlargeFont3 = document.getElementById("title3");
+var enlargeFont4 = document.getElementById("title4");
+
+
 enlargeFont.addEventListener("mouseover", function function1() {
     enlargeFont.style.scale = "1.05";
 });
 enlargeFont.addEventListener("mouseout", function function1() {
     enlargeFont.style.scale = "1.00";
 });
+
+enlargeFont1.addEventListener("mouseover", function function1() {
+    enlargeFont1.style.scale = "1.05";
+});
+enlargeFont1.addEventListener("mouseout", function function1() {
+    enlargeFont1.style.scale = "1.00";
+});
+
+enlargeFont2.addEventListener("mouseover", function function1() {
+    enlargeFont2.style.scale = "1.05";
+});
+enlargeFont2.addEventListener("mouseout", function function1() {
+    enlargeFont2.style.scale = "1.00";
+});
+
+enlargeFont3.addEventListener("mouseover", function function1() {
+    enlargeFont3.style.scale = "1.05";
+});
+enlargeFont3.addEventListener("mouseout", function function1() {
+    enlargeFont3.style.scale = "1.00";
+});
+
+enlargeFont4.addEventListener("mouseover", function function1() {
+    enlargeFont4.style.scale = "1.05";
+});
+enlargeFont4.addEventListener("mouseout", function function1() {
+    enlargeFont4.style.scale = "1.00";
+});
+
+
--- ts2.ts
+++
@@ -1,8 +0,0 @@
-var enlargeFont = <HTMLElement>document.getElementById("header");
-
-enlargeFont.addEventListener("mouseover", function function1() {
-    enlargeFont.style.scale = "1.05";
-})
-enlargeFont.addEventListener("mouseout", function function1() {
-    enlargeFont.style.scale = "1.00";
-})
